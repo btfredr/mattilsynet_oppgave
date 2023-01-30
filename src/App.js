8f63a8f3d633bbb387eaaf38b15aa59bffda116a
Fredrik <fredrikf2000@gmail.com>
2023-01-30 19:41:21 +0100

    started working on API fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <h1>Mattilsynet oppgave</h1>;
+  const [plasser, setPlasser] = useState([]);
+
+  useEffect(() => {
+    const fetchPlasser = async () => {
+      const url = `http://localhost:5000/produksjonsplass`;
+      // henter inn produksjonsplassene fra APIet
+      const result = await axios(url);
+
+      // setter innholdet fra APIet til konstanten
+      setPlasser(result.data);
+      console.log(result.data);
+    };
+
+    // kjører funksjonen
+    fetchPlasser();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Mattilsynet oppgave</h1>
+
+        <div className="produksjonsplasser">
+          {plasser.map((plass) => (
+            <div className="produksjonsplass" key={plass.produksjonsplassid}>
+              <p>{plass.kommunenummer}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
